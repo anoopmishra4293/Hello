bdcd319750ab2bda34568faa4b559117872b7d7a
anoopmishra4293 <anoopmishra4293@gmail.com>
2021-03-18 22:56:28 +0530

    added contact form

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 
@@ -29,4 +30,5 @@ def contact(request):
         desc = request.POST.get('desc')
         contact = Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
         contact.save()
+        messages.success(request, 'Your message has been sent!')
     return render(request, 'contact.html')
